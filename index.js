61c9dccd82b84fbcec293f7fee5ef3303bca65f3
IchalAlwiros <alwiros99@gmail.com>
2023-01-05 06:55:22 +0700

    setup mongo connect

--- index.js
+++ index.js
@@ -15,30 +15,37 @@ app.use(cors(corsOption))
 app.use(express.json())
 
 // connect DB
+db.mongoose.set('strictQuery', false);
+
 const mongooseConfig = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-// db.mongoose.createConnection(db.url, mongooseConfig).then(()=>{
-//     console.log("database connected");
-// }).catch(err => {
-//     console.log(`gagal connect ${err.message}`);
-//     process.exit();
-// })
+db.mongoose.connect(db.url, mongooseConfig).then(()=>{
+    console.log("database connected");
+}).catch(err => {
+    console.log(`gagal connect ${err.message}`);
+    process.exit();
+})
 
-db.mongoose.connect(db.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// db.mongoose.connect(db.url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-const connection = db.mongoose.connection;
-connection.on('error', console.error.bind(console, 'connection error:'));
-connection.once('open', function() {
-  console.log('Connected to MongoDB using Mongoose connect method!')
-});
+// const connection = db.mongoose.connection;
+// connection.on('error', console.error.bind(console, 'connection error:'));
+// connection.once('open', function() {
+//   console.log('Connected to MongoDB using Mongoose connect method!')
+// });
 
 // db.mongoose.createConnection(db.url).asPromise().then(()=>console.log('connect database')).catch(err => {console.log(`gagal connect ${err.massage}`); process.exit()})
 
+// Memanggil Route
+require("./routes")(app)
+
+
+// PORT
 const PORT = process.env.PORT || 8080
 app.listen(PORT, ()=>{
     console.log(`listen port ${PORT}`);
